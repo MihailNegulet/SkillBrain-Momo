0592a6211a5304e77ab34fd3ca9927cede516862
Mihail <mihail.negulet@gmail.com>
2024-10-03 16:19:39 +0300

    Second homework form JS - Intro

---
+++ Sprint 7-JS-Intro/tema2.js
@@ -0,0 +1,7 @@
+// Definim functia
+function tellFortune(N, Z, Y, X){
+    return `Vei fi un ${X} în ${Y}, căsătorit cu ${Z} și vei avea ${N} copii.`
+}
+
+// apelam functia
+console.log(tellFortune(3, "Emanuel", "Italia", "Programator"));
