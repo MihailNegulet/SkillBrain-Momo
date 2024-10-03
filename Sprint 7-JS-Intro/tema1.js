dbb4b4ed25fa6d8b5e729cccfb30c17d1d1d7f7e
Mihail <mihail.negulet@gmail.com>
2024-10-03 16:07:20 +0300

    First homework JavaScript Intro

---
+++ Sprint 7-JS-Intro/tema1.js
@@ -0,0 +1,8 @@
+// Definim functia
+function isDivided(x){
+    return x % 10 === 0;
+}
+
+// Afisam rezultatele
+console.log(`Numarul 100 este divizibil cu 10? ` + isDivided(100));
+console.log(`Numarul 23 este divizibil cu 10? ` + isDivided(23));
