6e0576cea56ec416e4d2d67b73964f728e29cf3a
Mihail <mihail.negulet@gmail.com>
2024-10-11 16:27:51 +0300

    third homework JS-arrays

---
+++ Sprint 8-Array&Loops/tema3.js
@@ -0,0 +1,8 @@
+// Creați o funcție care inversează un șir de numere
+
+const array1 = [1, -5, 20, -34, 16, 29, 36, -4];
+
+function reverseArray(array1) {
+    return array1.reverse();
+}
+console.log(reverseArray(array1));
