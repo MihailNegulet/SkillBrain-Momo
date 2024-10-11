2316fffcd070e09167f53088cd45c4ba3e295695
Mihail <mihail.negulet@gmail.com>
2024-10-11 16:00:50 +0300

    Homework number 2 from JS-Array

---
+++ Sprint 8-Array&Loops/tema2.js
@@ -0,0 +1,21 @@
+//Calculați suma numerelor dintr-un șir de numere
+
+// Exemplul unu
+const array1 = [1, -5, 20, -34, 16, 29, 36, -4]
+let output1 = 0;
+for (let i = 0; i < array1.length; i++) {
+    output1 = output1 + array1[i];
+}
+
+console.log(output1);
+
+// Exemplul 2
+
+const array2 = [3.45, -2.68, 356, -75.96, 64, 19.28]
+let output2 = 0;
+for (let i = 0; i < array2.length; i++) {
+    output2 = output2 + array2[i];
+}
+
+console.log(output2);
+
