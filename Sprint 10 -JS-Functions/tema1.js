08013108186b39411944523fbf272b9a240e3522
Mihail <mihail.negulet@gmail.com>
2024-10-24 20:28:24 +0300

    first homework

---
+++ Sprint 10 -JS-Functions/tema1.js
@@ -0,0 +1,9 @@
+// Scrieți o funcție care returnează pătratul unui număr
+// Funcția findSquare(num) returnează pătratul numărului primit ca date de intrare (este transmis ca 
+// argument în apelul funcției).
+
+getSquare = (num) => console.log(num*num);
+
+getSquare(6);
+getSquare(0);
+getSquare(-12);
